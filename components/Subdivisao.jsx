9d76669268fe50ee939c699f0d409545ae15db2c
Rafaela <rafaela.vm@gmail.com>
2021-11-21 20:53:18 -0300

    primeiro commit

---
+++ components/Subdivisao.jsx
@@ -0,0 +1,6 @@
+import styles from '../styles/Subdivisao.module.css'
+
+export default function SubDivisao(props){
+    
+    return (<div className={styles.subdivisao}>teste</div>)
+}
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import SubDivisao from '../components/Subdivisao'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <SubDivisao />
+      <SubDivisao />
+      <SubDivisao />
+      <SubDivisao />
+    </div>
+
+
+  )
+}
